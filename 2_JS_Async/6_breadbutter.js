66c321e6b62b54c27dc7920e2c333e194d12602a
PatrickHeuer <patrick-heuer@gmx.de>
2023-02-28 00:07:59 +0100

    extra exercise 2_6

--- 2_JS_Async/6_breadbutter.js
+++ 2_JS_Async/6_breadbutter.js
@@ -139,7 +139,7 @@ function yummy() {
 }
 
 let { openFridge, takeButter, takeBread, sliceBread, spreadButter } = 
-  require("./lib/actions_cb.js.js")(doAsync, doSilly, doThrow);
+  require("./lib/actions_cb.js")(doAsync, doSilly, doThrow);
 
 
 function breadAndButterCb() {
@@ -151,6 +151,22 @@ function breadAndButterCb() {
   console.log();
   
   // Write the async invocation code with callbacks here.
+  // openFridge(takeButter);
+  // takeButter();
+  //takeBread();
+  openFridge(() => {
+    takeButter(() => {
+      takeBread();
+      sliceBread(() => {
+        spreadButter();
+        yummy();
+      });
+    });
+  });
+  // sliceBread();
+  // spreadButter();
+
+  // yummy();
 }
 
 breadAndButterCb();
